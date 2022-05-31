ad9f236e13a788151bbb47347054e925c1f683b8
Felipe Gallego <felipegallego600@gmail.com>
2022-05-31 11:23:05 -0500

    casi2

--- src/Frontend/src/app/app.component.ts
+++ src/Frontend/src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'House';
   nombre:string = ""
   
 
-
+  constructor(){
+    
+  }
   ngOnInit(): void {
-    document.getElementById("About Us")!.style.display = "inline-block"
-    console.log("buenas")
+    //document.getElementById("About Us")!.style.display = "inline-block"
+    this.verificarlogin()
 
   }
 
@@ -26,14 +28,21 @@ export class AppComponent {
     window.location.replace("http://localhost:4200/aboutus");
   }
   
-  verificarlogin(){
-    let persona = JSON.parse( localStorage.getItem('persona')!)
+  verificarlogin():void{
+    console.log("asd")
+    let x=localStorage.getItem('persona')
+    
+    if (x!=null){
+    let persona = JSON.parse(x)
+    console.log(persona)
     if(persona){
+      
       this.nombre=persona.name;
       let element = document.getElementById("porfile")
       element!.style.display = "flex"
       let elementlogin = document.getElementById("login")
       elementlogin!.style.display = "none"
     }
+  }
   }
 }
